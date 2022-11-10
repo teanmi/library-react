71ad45bd3d7c2562d2f0c2c5479ac89509958ab7
teanmi <teanmi6@gmail.com>
2022-11-09 22:01:41 -0500

    added header and highlights

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Nav from "./components/nav"
+import Nav from "./components/Nav"
+import Landing from "./components/Landing"
+import Highlights from "./components/Highlights";
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
---
+++ src/components/ui/Highlight.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Highlight = ({ icon, subtitle, para }) => {
+  return (
+    <div className="highlight">
+      <div className="highlight__img">{icon}</div>
+      <h3 className="highlight__subtitle">{subtitle}</h3>
+      <p className="highlight__para">{para}</p>
+    </div>
+  );
+};
+
+export default Highlight;
